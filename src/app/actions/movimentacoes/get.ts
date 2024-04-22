952c27aea7c4f4d63650102b694a5750712cb100
joaocarloslima <joaocarloslima@me.com>
2024-04-22 09:37:34 -0300

    Fix API response handling in get.ts

--- src/app/actions/movimentacoes/get.ts
+++ src/app/actions/movimentacoes/get.ts
@@ -3,5 +3,6 @@
 export async function get() {
     await new Promise(r => setTimeout(r, 1000))
     const resp = await fetch(process.env.API_BASE_URL + "/movimentacao", { next: { revalidate: 0 } })
-    return await resp.json()
+    const json = await resp.json()
+    return json.content
 }
